12acbfb935761f486cd36bb11e3634cde43767d0
pragatikohli <pragatikohli12@gmail.com>
2022-08-03 22:16:03 +0530

    Completed JS

--- main.js
+++ main.js
@@ -2,7 +2,11 @@ const header = document.querySelector("header");
 const first_skill = document.querySelector(".skill:first-child");
 const sk_counters = document.querySelectorAll(".counter span");
 const progress_bars = document.querySelectorAll(".skills svg circle");
+const links = document.querySelectorAll(".nav-link");
+const togglebtn = document.querySelector(".toggle-btn");
+const hamburger = document.querySelector(".hamburger");
 window.addEventListener("scroll", () => {
+  activeLink();
   if (!skillsPlayed) skillsCounter();
 });
 // Sticky Navbar
@@ -49,3 +53,47 @@ function skillsCounter() {
     (p) => (p.style.animation = "progress 2s ease-in-out forwards")
   );
 }
+//Change Active Link on Scroll
+function activeLink() {
+  let sections = document.querySelectorAll("section[id]");
+  let passedSections = Array.from(sections)
+    .map((sct, i) => {
+      return {
+        y: sct.getBoundingClientRect().top - header.offsetHeight,
+        id: i,
+      };
+    })
+    .filter((sct) => sct.y <= 0);
+  let curSectionId = passedSections.at(-1).id;
+  links.forEach((l) => l.classList.remove("active"));
+  links[curSectionId].classList.add("active");
+}
+activeLink();
+//Dark Theme
+let firstTheme = localStorage.getItem("dark");
+changeTheme(+firstTheme);
+function changeTheme(isDark) {
+  if (isDark) {
+    document.body.classList.add("dark");
+    togglebtn.classList.replace("fa-moon", "fa-sun-bright");
+    localStorage.setItem("dark", 1);
+  } else {
+    document.body.classList.remove("dark");
+    togglebtn.classList.replace("fa-sun-bright", "fa-moon");
+    localStorage.setItem("dark", 0);
+  }
+}
+togglebtn.addEventListener("click", () => {
+  changeTheme(!document.body.classList.contains("dark"));
+});
+//Open and Close Navbar Menu
+hamburger.addEventListener("click", () => {
+  document.body.classList.toggle("open");
+  document.body.classList.toggle("stopScrolling");
+});
+links.forEach((link) =>
+  link.addEventListener("click", () => {
+    document.body.classList.remove("open");
+    document.body.classList.remove("stopScrolling");
+  })
+);
